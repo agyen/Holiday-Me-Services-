453e96999d5e45ff9edcaddb119796ab95e04808
agyen <izagyen96gmail.comgit config --global user.email izagyen96gmail.com>
2020-02-06 12:50:42 +0000

    adding travis file

---
+++ src/test/java/io/turntabl/Holiday/Me/HolidayMeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.turntabl.Holiday.Me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
